36f791f52304620affdb90d9e3638e5036a11050
Vishal Kushwaha <vishal@tdgpartner.info>
2019-12-07 17:19:57 +0530

    a

---
+++ build/precache-manifest.fb4eabbf2e73a39318f023fa09d424be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08e151da8a781906a11d54531545a49",
+    "url": "/tdg/index.html"
+  },
+  {
+    "revision": "ca3cea449056dbb203f1",
+    "url": "/tdg/static/css/main.7758f01b.chunk.css"
+  },
+  {
+    "revision": "99949e6d00633f98d62f",
+    "url": "/tdg/static/js/2.9aa35687.chunk.js"
+  },
+  {
+    "revision": "ca3cea449056dbb203f1",
+    "url": "/tdg/static/js/main.5f4e6231.chunk.js"
+  },
+  {
+    "revision": "7f24e443d3eea388b0bd",
+    "url": "/tdg/static/js/runtime~main.02245b68.js"
+  }
+]);
